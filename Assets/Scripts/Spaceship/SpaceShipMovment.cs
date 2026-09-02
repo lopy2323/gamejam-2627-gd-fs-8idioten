2661286b524ac2f533616828d6a1394377343c8f
Jonah Luijnenburg <38752@ma-web.nl>
2026-09-02 11:45:42 +0200

    added finalised spaceship movment. now working on visuals

--- Assets/Scripts/Spaceship/SpaceShipMovment.cs
+++ Assets/Scripts/Spaceship/SpaceShipMovment.cs
@@ -1,15 +1,30 @@
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SpaceShipMovment : MonoBehaviour
 {
+    [Header("Movement Input")]
+    [SerializeField] float thrustSpeed = 10f;
+    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] float boostPower = 10f;
+    [SerializeField] float dampeningFactor = 0.98f;
+
+    [SerializeField] float rotationLockDuration = 0.5f;
+    [SerializeField] float thrustinputWindow = 0.3f;
+
     private Vector2 velocity;
     private Vector2 position;
+
+    private bool lockRotation = false;
 
     private CharacterController controller;
     Vector2 rotationvector;
     bool isThrusting;
+
+    float ThrustTimer = 0f;
+    float rotationLockTimer = 0f;
 
     void Start()
     {
@@ -19,30 +34,45 @@ public class SpaceShipMovment : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        rotationvector = context.ReadValue<Vector2>();
+        if (rotationLockTimer <= rotationLockDuration) return;
+        Vector2 input = context.ReadValue<Vector2>();
+        if (input != Vector2.zero) rotationvector = input;
     }
 
     public void OnThrust(InputAction.CallbackContext context)
     {
-        if (context.performed) // the key has been pressed
+        if (context.performed)
         {
             isThrusting = true;
+            ThrustTimer = 0f;
         }
-        if (context.canceled) //the key has been released
+        if (context.canceled)
         {
+            if (ThrustTimer < thrustinputWindow)
+            {
+                float rotationAngle = Mathf.Atan2(rotationvector.y, rotationvector.x) * Mathf.Rad2Deg;
+                velocity += new Vector2(transform.right.x, transform.right.y) * boostPower;
+                rotationLockTimer = 0f;
+                ThrustTimer = 0f;
+            }
             isThrusting = false;
         }
     }
 
     void Update()
     {
-        float rotationAngle = Mathf.Atan2(rotationvector.y, rotationvector.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
+        ThrustTimer += Time.deltaTime;
+        rotationLockTimer += Time.deltaTime;
+        float targetRotationAngle = Mathf.Atan2(rotationvector.y, rotationvector.x) * Mathf.Rad2Deg;
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, targetRotationAngle), rotationSpeed * Time.deltaTime);
 
         if (isThrusting)
         {
-            velocity += new Vector2(Mathf.Cos(rotationAngle * Mathf.Deg2Rad), Mathf.Sin(rotationAngle * Mathf.Deg2Rad)) * 10f * Time.deltaTime;
+            velocity += new Vector2(transform.right.x, transform.right.y) * thrustSpeed * Time.deltaTime;
         }
+
+        velocity *= dampeningFactor;
 
         position += velocity * Time.deltaTime;
 
