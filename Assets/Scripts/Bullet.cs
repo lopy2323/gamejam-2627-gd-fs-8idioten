cd2c8f3ca30b6ca12ddca2b729ef17733d8b3647
R.D. Keeren <r.keeren@hotmail.com>
2026-09-02 14:19:59 +0200

    shooting is Done

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Collider2D originCollider;
     private float lifetime = 2f;
+    public int damage;
+    public Vector2 velocity = Vector2.zero;
     void Update()
     {
         lifetime -= Time.deltaTime;
@@ -10,6 +13,22 @@ public class Bullet : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        transform.position += transform.up * Time.deltaTime * 8f;
+        transform.position += (transform.right * 8f + new Vector3(velocity.x, velocity.y, 0)) * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider == originCollider)
+        {
+            return;
+        }
+        Debug.Log("ddd");
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health != null)
+        {
+            health.Damage(damage);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Healthbar/HealthbarUI.cs
+++ Assets/Scripts/Healthbar/HealthbarUI.cs
@@ -9,8 +9,6 @@ public class HealthbarUI : MonoBehaviour
 
     [SerializeField] private Image fillImage;
     [SerializeField] private Gradient healthGradient;
-
-    [SerializeField] private TMP_Text healthText;
     private void Start()
     {
         healthSlider.maxValue = playerHealth.MaxHealth;
@@ -34,6 +32,5 @@ public class HealthbarUI : MonoBehaviour
         (float)playerHealth.CurrentHealth / playerHealth.MaxHealth;
 
         fillImage.color = healthGradient.Evaluate(healthPercentage);
-        healthText.text = Mathf.RoundToInt(healthPercentage * 100) + "%";
     }
 }
